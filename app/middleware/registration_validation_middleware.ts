6e7272698fc1ed7733df6c43d615717db927eee8
renatho <renatho.20gomes@gmail.com>
2024-07-11 12:43:24 -0300

    middleware

--- app/middleware/registration_validation_middleware.ts
+++ app/middleware/registration_validation_middleware.ts
@@ -8,13 +8,13 @@ export default class RegistrationValidationMiddleware {
 
     if (!username || !email || !password) {
       return ctx.response.status(400).json({
-        message: 'Credenciais inválidas',
+        message: 'Por favor, verifique se todos os campos estão preenchidos corretamente.',
       })
     }
 
     if (typeof username !== 'string' || typeof email !== 'string' || typeof password !== 'string') {
       return ctx.response.status(400).json({
-        message: 'Credenciais inválidas',
+        message: 'Por favor, verifique se todos os campos estão preenchidos corretamente.',
       })
     }
 
